5b7d0ca1b398290a4cf6c8a1c5d939d91f022f8c
Andrey Demyanov <demyanov23@gmail.com>
2020-11-03 07:56:45 +0300

    Улучшает работу проверку количества комнат и гостей

--- js/main.js
+++ js/main.js
@@ -182,10 +182,10 @@ const housingGuests = document.querySelector('#capacity');
 const housingRooms = document.querySelector('#room_number');
 
 housingGuests.addEventListener('change', function () {
-  const guestsAmount = housingGuests[housingGuests.selectedIndex].value;
+  const guestsAmount = Number(housingGuests[housingGuests.selectedIndex].value);
 
   for (let i = 0; i < housingRooms.length; i++) {
-    if ((guestsAmount > Number(housingRooms[i].value)) || guestsAmount === '0') {
+    if (guestsAmount > housingRooms[i].value || guestsAmount === 0) {
       housingRooms[i].setAttribute('disabled', 'true');
     } else {
       housingRooms[i].removeAttribute('disabled');
